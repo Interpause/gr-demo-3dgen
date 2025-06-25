5e3b0c6a195954c6a4d1cd93027a923f9783a489
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2025-06-25 23:20:59 +0800

    feat: describe sketch with optional voice command

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from pathlib import Path
 import gradio as gr
 from dotenv import load_dotenv
 
-from gr_demo_3dgen.groq import groq_create_client, groq_transcribe_audio
+from gr_demo_3dgen.groq import (
+    groq_create_client,
+    groq_describe_image,
+    groq_transcribe_audio,
+)
 from gr_demo_3dgen.utils import np_wav_to_compressed_buffer
 
 load_dotenv()
@@ -18,33 +22,65 @@ HEAD = """
 
 
 with gr.Blocks(head=HEAD) as demo:
-    gr.Markdown("# Greetings from Gradio!")
+    gr.Markdown("# SDS Group 3 3DGen Props Demo")
 
     with gr.Row():
-        input_audio = gr.Audio(
-            label="Voice Command",
-            type="numpy",
-            streaming=False,
-        )
-        input_text = gr.Textbox(
-            label="Optional Text Guidance",
-            placeholder="Optionally tell agent what you sketched. Voice Command will override this.",
-            interactive=True,
-            lines=2,
-        )
+        with gr.Column():
+            input_text = gr.Textbox(
+                label="Optional Text Guidance",
+                placeholder="Optionally tell agent what you sketched. Voice Command will override this.",
+                interactive=True,
+                lines=3,
+            )
+            preview_prompt = gr.Textbox(
+                label="Prompt Preview",
+                placeholder="The prompt that will be sent to the AI art model.",
+                interactive=False,
+                lines=3,
+            )
+        with gr.Column():
+            input_audio = gr.Audio(
+                label="Voice Command",
+                sources=["microphone", "upload"],
+                type="numpy",
+                streaming=False,
+            )
+            submit_button = gr.Button(
+                "Generate 3D Model",
+                variant="primary",
+                elem_id="submit-button",
+            )
 
-        def test(val):
-            sr, wav = val
-            audio_buf = np_wav_to_compressed_buffer(sr, wav)
-            text = groq_transcribe_audio(groq_client, audio_buf)
-            return text
+    input_sketch = gr.ImageEditor(
+        label="Sketch",
+        image_mode="RGB",
+        type="pil",
+        canvas_size=(768, 768),
+        layers=False,
+        height="50vh",
+    )
 
-        input_audio.input(test, inputs=input_audio, outputs=input_text)
+    @gr.on([input_audio.input], inputs=input_audio, outputs=input_text)
+    def on_voice_input(val):
+        sr, wav = val
+        audio_buf = np_wav_to_compressed_buffer(sr, wav)
+        text = groq_transcribe_audio(groq_client, audio_buf)
+        return text
 
-    inp = gr.Textbox(placeholder="What is your name?")
-    out = gr.Textbox()
+    @gr.on(
+        [submit_button.click], inputs=[input_text, input_sketch], outputs=preview_prompt
+    )
+    def on_submit(text, sketch):
+        text = text.strip()
+        if len(text) == 0:
+            text = None
+        prompt = groq_describe_image(groq_client, sketch["composite"], description=text)
+        return prompt
 
-    inp.change(fn=lambda x: f"Welcome, {x}!", inputs=inp, outputs=out)
+    @gr.on([preview_prompt.change], inputs=[preview_prompt, input_sketch])
+    def on_prompt_change(prompt, sketch):
+        pass
+
 
 if __name__ == "__main__":
     demo.launch()
